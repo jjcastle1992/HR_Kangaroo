fec94dc0527eb5ee463c04342c8178f37a6aacb8
James <jjcastle@utexas.edu>
2022-06-08 21:47:19 -0500

    Added condition where if the two kangaroos start at the different places, but have the same rate of change, then automatically return that a match is impossible (as positions must always be different for each iteration).

--- main.cpp
+++ main.cpp
@@ -29,6 +29,9 @@ string kangaroo(int x1, int v1, int x2, int v2) {
     if ((x1 > x2 && v1 > v2) || (x1 < x2 && v1 < v2)) {
         positionMatch = false;
     }
+    else if (((x1 > x2) && (v1 == v2)) || (x2 > x1) && (v2 == v1)) {
+        positionMatch = false;
+    }
     else {
         if (kangaroo1Position == kangaroo2Position ) {
             positionMatch = true;
